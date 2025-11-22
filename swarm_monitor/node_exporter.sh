d472c02bf7d772bdb04df7a2d0514e9d4a714bed
Oscar Tran <quanghuypthiet@gmail.com>
2025-11-22 20:12:59 +0700

    Update docker swarm

--- swarm_monitor/node_exporter.sh
+++ swarm_monitor/node_exporter.sh
@@ -4,7 +4,7 @@ tar -xvf node_exporter-1.0.1.linux-amd64.tar.gz
 mv node_exporter-1.0.1.linux-amd64 node_exporter-files
 sudo cp node_exporter-files/node_exporter /usr/bin/
 
-sudo tee -a /etc/systemd/system/node_exporter.service > /dev/null <<EOT
+sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOT
 [Unit]
 Description=Node Exporter
 Documentation=https://prometheus.io/docs/guides/node-exporter/
@@ -12,15 +12,20 @@ Wants=network-online.target
 After=network-online.target
 
 [Service]
-User=node_exporter
-Group=node_exporter
 Type=simple
 Restart=on-failure
+ExecStart=
 ExecStart=/usr/bin/node_exporter \
-  --web.listen-address=:9100
+  --web.listen-address=:9100 \
+  --collector.textfile.directory=/var/lib/node-exporter
 
 [Install]
 WantedBy=multi-user.target
+EOT
+
+sudo mkdir -p /var/lib/node-exporter
+sudo tee /var/lib/node-exporter/node.prom > /dev/null << EOT
+node{node="$(hostname)"} 1
 EOT
 
 sudo systemctl daemon-reload
